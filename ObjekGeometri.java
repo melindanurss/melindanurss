c93b2d6c6999751941aece77b4d9da7dda8d7248
melindanurss <156186177+melindanurss@users.noreply.github.com>
2024-01-10 18:16:48 +0700

    Create ObjekGeometri.java

---
+++ ObjekGeometri.java
@@ -0,0 +1,10 @@
+public class ObjekGeometri {
+
+    String Color = "red";
+    public void setColor(String Color){
+        this.Color = Color;
+    }
+    public String getColor(){
+        return this.Color;
+    }
+}
